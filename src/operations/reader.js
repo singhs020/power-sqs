85fdddc6bb7136002f9f5dfa6d3d8a86c57c732f
Simranjeet Singh <simranjeet2589@gmail.com>
2019-06-07 15:59:34 +0530

    [Adding a basic reader]

---
+++ src/operations/reader.js
@@ -0,0 +1,30 @@
+const AWS = require("aws-sdk");
+const {Readable} = require("stream");
+
+const SQS = new AWS.SQS({
+  "apiVersion": "2012-11-05"
+});
+
+class Reader extends Readable {
+  constructor({url, logger}){
+    super({"objectMode": true});
+
+    this._logger = logger;
+    this._params = {
+      "QueueUrl": url
+    };
+  }
+  _read(){
+    SQS.receiveMessage(this._params, (err, data) => {
+      if(err) {
+        this._logger.error("There was an error while reading data from SQS", err);
+      }
+
+      console.log(data);
+
+      this.push(JSON.stringify(data));
+    });
+  }
+}
+
+module.exports = Reader;

d56f31983f95e9eb33baffbf1174553bd3e3c163
Simranjeet Singh <simranjeet2589@gmail.com>
2019-06-07 16:29:16 +0530

    [setting up gulp and eslint]

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const {src, series} = require("gulp");
+const eslint = require("gulp-eslint");
+
+
+function lint() {
+  return src(["src/*.js"])
+  .pipe(eslint())
+  .pipe(eslint.failAfterError());
+}
+
+exports.default = series([lint]);
+exports.lint = lint;

c454f43b60363d3bb1e57e97550717affe5e053f
Simranjeet Singh <simranjeet2589@gmail.com>
2019-06-11 11:35:58 +0530

    [convert wirte to a transform stream for remover]

--- src/operations/writer.js
+++ src/operations/writer.js
@@ -1,49 +1,30 @@
 const assert = require("assert");
-const {Writable} = require("stream");
+const {Transform} = require("stream");
+const pino = require("pino");
 
-class Writer extends Writable {
-  constructor({provider, sourceUrl, logger, sqs}){
+class Writer extends Transform {
+  constructor({provider, logger}){
     super({
       "objectMode": true,
       "highWatermark": 10
     });
 
     assert(provider, "provider is required");
-    assert(sourceUrl, "sourceUrl is required");
-    assert(sqs, "sqs is required");
     assert(logger, "logger is required");
 
     this._logger = logger;
     this._provider = provider;
-    this._sourceUrl = sourceUrl;
-    this._sqs = sqs;
   }
 
-  _deleteMessages(data) {
-    const messages = data.Messages.map(item => ({"Id": item.MessageId, "ReceiptHandle": item.ReceiptHandle}));
-
-    const params = {
-      "QueueUrl": this._sourceUrl,
-      "Entries": messages
-    };
-
-    return new Promise((resolve, reject) => {
-      this._sqs.deleteMessageBatch(params, (err, data) => {
-        if(err) {
-          return reject(err);
-        }
-
-        resolve(data);
-      });
-    });
-  }
-
-  _write(chunk, encoding, cb) {
+  _transform(chunk, encoding, cb) {
     return this._provider.sink(chunk)
-      .then(() => this._deleteMessages(chunk))
-      .then(() => cb())
+      .then(() => this.push(chunk))
       .catch(err => cb(err));
   }
 }
 
-module.exports = Writer;
+module.exports = provider => {
+  const logger = pino({"name": "Writer"});
+
+  return new Writer({provider, logger});
+};
